d4af4e296bf324b0b5769ac2536671a87395ea8b
trisberg <trisberg@pivotal.io>
2018-11-05 11:18:10 -0500

    Add uppercase app with Gradle build

---
+++ src/main/java/com/example/uppercase/UppercaseApplication.java
@@ -0,0 +1,20 @@
+package com.example.uppercase;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class UppercaseApplication {
+
+	@Bean
+	Function<String, String> uppercase() {
+		return (in) -> in.toUpperCase()+"!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppercaseApplication.class, args);
+	}
+}
